d19bb877e1df4ca3aaa2988808f1ac8fc1d05d26
jungpill <ljp10777@gmail.com>
2025-03-27 16:02:03 +0900

    create: ProfileImage.tsx

--- src/pages/myProfile/MyProfile.tsx
+++ src/pages/myProfile/MyProfile.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components"
 import MyProfileHeader from "./Header"
-
+import ProfileImageWrapper from './ProfileImage'
 
 const MyProfile = () => {
     return(
         <MyProfileContainer>
             <MyProfileHeader/>
+            <ProfileImageWrapper/>
         </MyProfileContainer>
     )
 }
@@ -14,10 +15,11 @@ export default MyProfile
 
 const MyProfileContainer = styled.div`
     display: flex;
-    width: 30%;
+    width: 20%;
     height: 90%;
     background-color: white;
     border-radius: 25px;
-    padding: 30px;
+    padding: 10px 30px 10px 30px;
     margin-right: 2%;
+    flex-direction: column;
 `
---
+++ src/pages/myProfile/ProfileImage.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import image from '../../source/사진 푸른배경.jpg'
+
+const ProfileImage = () => {
+
+
+    return(
+        <ProfileImageWrapper>
+            <Image src = {image}/>
+        </ProfileImageWrapper>
+    )
+}
+
+export default ProfileImage
+
+const ProfileImageWrapper = styled.div`
+    width: 100%;
+    height: 230px;
+    
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+`
