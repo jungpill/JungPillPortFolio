d226ccce7b3ca9fdfc3d2432a868507a4bcad4d6
jungpill <ljp10777@gmail.com>
2025-03-30 19:13:14 +0900

    create: ImageCard.tsx

---
+++ src/component/ImageCard.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+const ImageCard = () => {
+
+
+
+    return(
+        <ImageCardContainer>
+            
+        </ImageCardContainer>
+    )
+}
+
+export default ImageCard
+
+
+const ImageCardContainer = styled.div`
+    display: flex;
+`
