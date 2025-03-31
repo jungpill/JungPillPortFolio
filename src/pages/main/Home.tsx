afba84a1acddf801333e6a19dd11972429fa018e
jungpill <ljp10777@gmail.com>
2025-04-01 00:51:27 +0900

    feat: 말풍선 추가

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -7,6 +7,7 @@ const HOME = () => {
     return(
         <BodyContainer>
             <Img src = {싸이월드}/>
+            <Balloon>유저의 입장에서 생각하며 최선의 방향을 고민하는 개발자 이정필입니다.</Balloon>
         </BodyContainer>
     )
 }
@@ -23,3 +24,34 @@ const Img = styled.img`
     width: 100%;
     height: 100%;
 `
+
+const Balloon = styled.div`
+  position: absolute;
+  background: #00aabb;
+  color: white;
+  padding: 30px;
+  border-radius: 12px;
+  max-width: 450px;
+  height: 50px;
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-align: center;
+  top: 35%;
+  left: 50%;
+
+  /* 말풍선 위쪽 삼각형 */
+  &::after {
+   content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-top-color: #00aabb;
+    border-bottom: 0;
+    border-left: 0;
+    margin-left: -10px;
+    margin-bottom: -20px;
+  }
+`;
