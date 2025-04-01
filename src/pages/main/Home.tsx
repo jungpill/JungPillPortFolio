327440d683deb02c36ea7f913a430e242f864cbe
jungpill <ljp10777@gmail.com>
2025-04-01 17:40:08 +0900

    7000포트와 연결

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -4,10 +4,31 @@ import styled from 'styled-components'
 
 const HOME = () => {
 
+
+   const handleTest = () => {
+    fetch('http://localhost:7000/visitor/increment', {
+        method: 'POST', // POST 방식으로 방문자 수를 증가시킨다고 가정
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // 응답이 성공적이면 JSON 데이터 반환
+        }
+        throw new Error('Failed to increment visitor count');
+    })
+    .then(data => {
+        console.log('Visitor count incremented:', data);
+        // 방문자 수가 증가한 후, 필요한 처리를 여기서 할 수 있음 (예: UI 업데이트)
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // 에러가 발생하면 사용자에게 알림을 주거나 에러 처리 로직을 추가할 수 있음
+    });
+}
+
     return(
         <BodyContainer>
             <Img src = {싸이월드}/>
-            <Balloon>유저의 입장에서 생각하며 최선의 방향을 고민하는 개발자 이정필입니다.</Balloon>
+            <Balloon onClick = {handleTest}>유저의 입장에서 생각하며 최선의 방향을 고민하는 개발자 이정필입니다.</Balloon>
         </BodyContainer>
     )
 }
@@ -27,16 +48,16 @@ const Img = styled.img`
 
 const Balloon = styled.div`
   position: absolute;
-  background: #00aabb;
+  background: #553DF2;
   color: white;
   padding: 30px;
   border-radius: 12px;
   max-width: 450px;
   height: 50px;
-  font-size: 1.5rem;
-  font-weight: 700;
+  font-size: 1.6rem;
+  font-weight: 500;
   text-align: center;
-  top: 35%;
+  top: 37%;
   left: 50%;
 
   /* 말풍선 위쪽 삼각형 */
@@ -48,7 +69,7 @@ const Balloon = styled.div`
     width: 0;
     height: 0;
     border: 20px solid transparent;
-    border-top-color: #00aabb;
+    border-top-color: #553DF2;
     border-bottom: 0;
     border-left: 0;
     margin-left: -10px;
