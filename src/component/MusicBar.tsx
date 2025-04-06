18e4a7b9b68d7a139bdf1147b1ea6b24e3830ac4
jungpill <ljp10777@gmail.com>
2025-04-07 00:43:32 +0900

    feat: MusicBar.tsx 디지인 완성

--- src/component/MusicBar.tsx
+++ src/component/MusicBar.tsx
@@ -1,14 +1,26 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import stop from '../../src/source/webp/stop.webp'
+import next from '../../src/source/webp/next.webp'
+import squeare from '../../src/source/webp/square.webp'
+import play from '../../src/source/webp/play.webp'
+import { useState } from "react";
 
 const MusicBar = () => {
 
+    const [active, setActive] = useState<boolean>(true);
 
     return(
         <MusicBarContainer>
             <TitleBox>
                 거북이
             </TitleBox>
+            <IconBox>
+                <Icon src = {active ? play : squeare}/>
+                <Icon src = {stop}/>
+                <Icon src = {next} style = {{transform: 'rotate(180deg)'}} />
+                <Icon src = {next}/>
+            </IconBox>
         </MusicBarContainer>
     )
 }
@@ -24,7 +36,7 @@ const MusicBarContainer = styled(motion.div)`
     width: 8rem;
     height: 3.5rem;
     background-color: darkgray;
-    right: 1%;
+    right: 0.8%;
     top: 10%;
     font-size: 10px;
 `
@@ -37,4 +49,15 @@ const TitleBox = styled.div`
     width: 90%;
     border: 1px solid black;
     background-color: white;
+`
+
+const IconBox = styled.div`
+    display: flex;
+    padding-top: 0.3px;
+`
+
+const Icon = styled.img`
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
 `
